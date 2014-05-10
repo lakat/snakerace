f1e47b7cea3ff36757ea0e8d8b98bac13ace2935
Mate Lakat <mlakat@mlakat.hu>
2014-05-10 21:03:58 +0100

    Move examples around
    related to lakat/snakerace#8

--- examples/some_module.py
+++
@@ -1,9 +0,0 @@
-class SomeClass(object):
-    def __init__(self, name):
-        self.name = name
-
-    def operation(self):
-        print self.name
-
-    def unexecuted(self):
-        pass
--- exercise.sh
+++
@@ -1,6 +0,0 @@
-# Get the covered lines of examples/some_module.py
-sr-getlines --sources examples/some_module.py --args examples/sample.py
-
-cd examples
-echo "racey_module:8" | sr-tournament snakerace.tests.example_race:SampleTournament
-

23c1dfd3e64ad726ac968934df3581cf5bcd5042
Mate Lakat <mlakat@mlakat.hu>
2014-05-11 22:44:59 +0100

    Add Simple ui
    fixes lakat/snakerace#17

--- snakerace/scripts.py
+++ snakerace/scripts.py
@@ -31,18 +31,30 @@ def parse_tournament_params(args):
     return parser.parse_args(args)
 
 
+def parse_cat_params(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filenames', nargs='*', help='Filenames to cat', default=['-'])
+
+    return parser.parse_args(args)
+
+
+def open_file_or_stdin(fname):
+    if fname == '-':
+        return sys.stdin
+    else:
+        return open(fname, 'rb')
+
+
 def break_run_continue():
     params = parse_tournament_params(sys.argv[1:])
     race_class = get_race(params.race)
-    if params.linespec_file == '-':
-        linespec_source = sys.stdin
-    else:
-        linespec_source = open(params.linespec_file, 'rb')
 
-    linespecs = [
-        linespec.parse(line)
-        for line in linespec_source.readlines()
-    ]
+    linespec_source = open_file_or_stdin(params.linespec_file)
+
+    linespecs = linespec.parse_lines(
+        linespec_source.readlines()
+    )
 
     result = tournament.run_tournament(
         linespecs, race_class(), sys.stdout)
@@ -74,3 +86,30 @@ def get_race(path_to_callable):
     module_name, function_name = path_to_callable.split(':')
     module = importlib.import_module(module_name)
     return getattr(module, function_name)
+
+
+def cat_main():
+    import termcolor
+    params = parse_cat_params(sys.argv[1:])
+
+    linespecs = []
+    for fname in params.filenames:
+        fhandle = open_file_or_stdin(fname)
+        linespecs = linespec.parse_lines(fhandle.readlines())
+        fhandle.close()
+
+    for linespecs_for_file in linespec.group(linespecs):
+        highlighted_lines = [ls.lineno for ls in linespecs_for_file]
+        fname = linespecs_for_file[0].fname
+
+        with open(fname, 'rb') as fhandle:
+            lines = fhandle.read().split('\n')
+
+        for idx, line in enumerate(lines):
+            formatted_line = termcolor.colored(
+                line,
+                'yellow' if idx + 1 in highlighted_lines else 'white'
+                ) + termcolor.colored('\n', 'white')
+
+            sys.stdout.write(formatted_line)
+
